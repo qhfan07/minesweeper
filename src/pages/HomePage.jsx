42150692cfd9e81bfd6d384bbaf6d32b4c5944be
qhfan07 <qhfan07@gmail.com>
2024-11-13 18:36:36 -0800

    second

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,10 +6,8 @@ const HomePage = () => (
   <div className="homepage">
     <h1>Welcome to Minesweeper</h1>
     <p>Navigate to start a new game or read the rules!</p>
-    <nav>
-      <Link to="/rules" className="homepage-link">View Rules</Link>
-      <Link to="/game/easy" className="homepage-link">Start Game</Link>
-    </nav>
+    <Link to="/rules" className="homepage-link">View Rules</Link>
+    <Link to="/game/easy" className="homepage-link">Start Game</Link>
   </div>
 );
 
